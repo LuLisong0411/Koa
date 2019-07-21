9149ce27fd8d9de7f689126af17dedaf58045334
陆力松 <lulisong0411@163.com>
2019-07-21 18:51:15 +0800

    ejs

---
+++ demo_12.js
@@ -0,0 +1,16 @@
+const Koa = require('koa');
+const views = require('koa-views');
+const path = require('path')
+const app = new Koa();
+
+app.use(views(path.join(__dirname,'./view'),{
+    extension:'ejs',
+}))
+
+app.use(async(ctx)=>{
+    let title = 'Hello Koa';
+    await ctx.render('index',{title})
+});
+app.listen(3000,()=>{
+    console.log('[demo] server is starting in port 3000')
+})
